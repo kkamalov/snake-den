de61a528e2afa1ef7dce85c15b3bb66f2948b60a
Kainar Kamalov <kainar.kamalov@gmail.com>
2016-12-17 05:03:42 +0000

    Added leaderboard.

--- client.js
+++ client.js
@@ -1,7 +1,8 @@
 config = {
   'roomServer': 'ws://199.21.79.248:444/slither',
   'mapServer': 'ws://websocket_server:8000',
-  'color': 'red'
+  'color': 'red',
+  'name': 'kainar'
 }
 
 ws = new WebSocket(config.roomServer)
@@ -15,24 +16,84 @@ if (typeof denSocket !== 'undefined') {
   }
 }
 
+// Setup init.
 denSocket = new WebSocket(config.mapServer);
 denSnakes = {}
+
+// Setup leaderboard.
+var denLbh = document.createElement("div");
+denLbh.className = "nsi";
+denLbh.style.position = "fixed";
+denLbh.style.left = "4px";
+denLbh.style.top = "4px";
+denLbh.style.textAlign = "center";
+denLbh.style.width = "255px";
+denLbh.style.height = "28px";
+denLbh.style.color = "#ffffff";
+denLbh.style.fontFamily = 'Arial, "Helvetica Neue", Helvetica, sans-serif';
+denLbh.style.fontSize = "21px";
+denLbh.style.fontWeight = "bold";
+denLbh.style.overflow = "hidden";
+denLbh.style.opacity = .7;
+denLbh.style.zIndex = 7;
+denLbh.style.cursor = "default";
+var lstr = "Den Leaderboard";
+"de" == lang ? lstr = "Bestenliste" : "fr" == lang ? lstr = "Gagnants" : "pt" == lang && (lstr = "L\u00edderes");
+denLbh.textContent = lstr;
+trf(denLbh, agpu);
+document.body.appendChild(denLbh);
+var denLbs = document.createElement("div");
+denLbs.className = "nsi";
+denLbs.style.position = "fixed";
+denLbs.style.textAlign = "center";
+denLbs.style.left = "20px";
+denLbs.style.top = "32px";
+denLbs.style.width = "150px";
+denLbs.style.height = "800px";
+denLbs.style.color = "#ffffff";
+denLbs.style.fontFamily = 'Arial, "Helvetica Neue", Helvetica, sans-serif';
+denLbs.style.fontSize = "12px";
+denLbs.style.overflow = "hidden";
+denLbs.style.opacity = .7;
+denLbs.style.zIndex = 7;
+denLbs.style.cursor = "default";
+denLbs.style.lineHeight = "150%";
+trf(denLbs, agpu);
+document.body.appendChild(denLbs);
+// End Setup Leaderboard
 
 updateLocation = function() {
   denSocket.send(JSON.stringify({
     top: myloc.style.top,
     left: myloc.style.left,
-    color: config.color
+    color: config.color,
+    name: config.name
+    // Internal score representation
+    sct: snake.sct,
+    fam: snake.fam
   }))
 }
 
+updateLeaderboard = function() {
+  var scores = "";
+  for (var denSnakeId in denSnakes) {
+    if (!denSnakes.hasOwnProperty(denSnakeId)) continue;
+
+    var denSnake = denSnakes[denSnakeId];
+
+    scores += '<span style="opacity:.7; color:' + denSnake.color + ';">' + denSnake.name + ': ' + denSnake.score + "</span><BR>";
+  }
+  denLbs.innerHTML = scores;
+}
+
 setInterval(updateLocation, 1000)
+setInterval(updateLeaderboard, 1000)
 
 denSocket.onmessage = function(e) {
   var data = JSON.parse(e.data);
   if (data.event === 'delete') {
-    var denLoc = denSnakes[data.id];
-    denLoc.remove();
+    var denSnake = denSnakes[data.id];
+    denSnake.remove();
     delete denSnakes[data.id];
     return;
   }
@@ -41,8 +102,9 @@ denSocket.onmessage = function(e) {
     var denSnake = denSnakes[data.id];
     denSnake.style.top = data.top;
     denSnake.style.left = data.left;
+    denSnake.score = Math.floor(15 * (fpsls[data.sct] + data.fam / fmlts[data.sct] - 1) - 5) / 1
   } else {
-    denLoc = document.createElement("img");
+    denSnake = document.createElement("img");
     lc.width = lc.height = 14;
     ctx = lc.getContext("2d");
     ctx.fillStyle = data.color;
@@ -52,15 +114,17 @@ denSocket.onmessage = function(e) {
     ctx.arc(7, 7, 2.5, 0, pi2);
     ctx.stroke();
     ctx.fill();
-    denLoc.src = lc.toDataURL();
-    denLoc.className = "nsi";
-    denLoc.style.position = "absolute";
-    denLoc.style.left = data.left;
-    denLoc.style.top = data.top;
-    denLoc.style.opacity = 1;
-    denLoc.style.zIndex = 13;
-    denSnakes[data.id] = denLoc;
+    denSnake.src = lc.toDataURL();
+    denSnake.className = "nsi";
+    denSnake.style.position = "absolute";
+    denSnake.style.left = data.left;
+    denSnake.style.top = data.top;
+    denSnake.style.opacity = 1;
+    denSnake.style.zIndex = 13;
+    denSnake.name = data.name
+    denSnake.color = data.color
+    denSnakes[data.id] = denSnake;
     trf(myloc, agpu);
-    loch.appendChild(denLoc);
+    loch.appendChild(denSnake);
   }
 }
